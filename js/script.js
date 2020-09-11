8cc573767378070d79ab84db0265d0928ca3789e
Elijas <49196132+elijaspakalnis@users.noreply.github.com>
2020-09-11 17:53:06 +0100

    add t-shirt info logic

--- js/script.js
+++ js/script.js
@@ -18,3 +18,56 @@ titleField.addEventListener('change', e => {
   }
 });
 
+// == T-Shirt Info ==
+// get reference to shirtcolor select options and select theme option from design select
+const shirtColors = document.querySelectorAll('#color option');
+const selectThemeOption = document.querySelector('option[value="select theme"]');
+
+// initial select theme option should hide all colors
+if(selectThemeOption.selected) {
+  // hide all colors and leave out default option
+  shirtColors.forEach(color => {
+    if(!/Please select/.test(color.textContent)) 
+      color.hidden = true;
+  });
+}
+
+//get reference to design select element
+const designSelectElement = document.getElementById('design');
+
+// onchange event listener for the design select element
+designSelectElement.addEventListener('change', (e) => {
+  const option = e.target.value;
+  
+  switch(option) {
+    case 'js puns': 
+      displayAvailableColors(/JS Puns/);
+      break;
+    case 'heart js':
+      displayAvailableColors(/I ♥ JS/);
+      break;
+    default:
+      displayAvailableColors(/Please select/);
+  }
+});
+
+// function to display available colors for design option
+const displayAvailableColors = option => {
+  // initially hide all color options
+  shirtColors.forEach(color => color.hidden = true);
+
+  let firstMatchFound = false;
+  for(let i = 0; i < shirtColors.length; i++) {
+    // check if option matches color
+    if(option.test(shirtColors[i].textContent)){
+      shirtColors[i].hidden = false; //display the color option
+      // check for first match and set it to selected state
+      if(!firstMatchFound) {
+        shirtColors[i].selected = true;
+        firstMatchFound = true;
+      }
+    }
+  }
+}
+
+
